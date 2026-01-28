b27f2960958d328bd77e21a08829840dd5dae7c7
guilherme <guibferrarezi@gmail.com>
2026-01-28 14:58:03 -0300

    turmas

--- web/src/pages/TurmaDetail.tsx
+++ web/src/pages/TurmaDetail.tsx
@@ -3,7 +3,6 @@ import { useParams, useNavigate } from "react-router-dom";
 import DashboardLayout from "../components/Dashboard/DashboardLayout";
 import {
   obterTurma,
-  adicionarAlunosNaTurma,
   removerAlunoDaTurma,
   getRole,
   type Turma,
