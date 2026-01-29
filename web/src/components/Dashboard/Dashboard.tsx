60d0c91308042ce26707a1b29ee3fcae054b25af
guilherme <guibferrarezi@gmail.com>
2026-01-29 13:19:22 -0300

    exercicisos

--- web/src/components/Dashboard/Dashboard.tsx
+++ web/src/components/Dashboard/Dashboard.tsx
@@ -390,7 +390,7 @@ export default function Dashboard() {
                 target.style.background = "white";
               }}
             >
-              Turmas
+              🏫 Turmas
             </button>
 
             {canCreateUser && (
